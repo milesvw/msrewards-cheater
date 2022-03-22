8562ef0e7eb0c2428f4974e17462e8d31f63aeb5
Miles <milesvenderwilson@gmail.com>
2022-03-22 14:24:50 -0400

    Refactor: make the creation of a search URL a function to make it more easily reused and cleaner

--- msrewards-cheater.py
+++ msrewards-cheater.py
@@ -3,16 +3,20 @@ import webbrowser, random, time
 # Set the base URL to be concatenated with a search word
 baseurl = "https://www.bing.com/search?q="
 
-# Open 30 tabs searching Bing with a different word each time
-for i in range(32):
+def url():
     with open("words.txt", "r", encoding="utf-8") as file:
         allText = file.read()
         words = allText.split()
         word = random.choice(words)
-        url = baseurl + word
-        
-        # Open the URL in a new tab in the browser
-        webbrowser.open(url)
 
-	    # Give time for the page to load
-        time.sleep(1)
+        url = baseurl + word
+        return url
+
+# Open 30 tabs searching Bing with a different word each time
+for i in range(5):
+
+    # Open the URL in a new tab in the browser
+    webbrowser.open(url())
+
+    # Give time for the page to load
+    time.sleep(1)
