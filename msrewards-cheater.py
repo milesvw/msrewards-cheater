7fc73a69c3e3851637972c1a869f01a944c2982a
Miles <milesvenderwilson@gmail.com>
2022-03-22 14:32:53 -0400

    Refactor: Make comments clearer and make the baseurl variable local to url()  instead of global

--- msrewards-cheater.py
+++ msrewards-cheater.py
@@ -1,18 +1,19 @@
 import webbrowser, random, time
 
-# Set the base URL to be concatenated with a search word
-baseurl = "https://www.bing.com/search?q="
-
+# Create a URL that searches Bing for a random word picked from a wordlist
 def url():
     with open("words.txt", "r", encoding="utf-8") as file:
         allText = file.read()
         words = allText.split()
         word = random.choice(words)
 
+        # Set the base URL to be concatenated with a search word
+        baseurl = "https://www.bing.com/search?q="
+
         url = baseurl + word
         return url
 
-# Open 30 tabs searching Bing with a different word each time
+# Open tabs searching Bing
 for i in range(5):
 
     # Open the URL in a new tab in the browser
